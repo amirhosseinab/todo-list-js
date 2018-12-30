0299ac586bc61078c57e631aa87a9e19a3acf3a9
amirhosseinab <ah.abdolkhalegh@gmail.com>
2018-12-30 17:23:05 +0330

    init commit
    implementing almost all features except editing.

---
+++ app.js
@@ -0,0 +1,80 @@
++function () {
+    let todos = [];
+    const addButton = document.getElementById("addButton");
+    const todoText = document.getElementById("todoText");
+    const todoList = document.getElementById("todoList");
+
+    addButton.onclick = addItem;
+    todoText.onkeypress = function (e) {
+        if (e.key === 'Enter') {
+            addItem()
+        }
+    };
+
+
+    function addItem() {
+        todos.push({text: todoText.value, done: false, editMode: false});
+        todoText.value = "";
+        refreshTodos();
+    }
+
+    function createTodoItem(todo, index) {
+        let item = document.createElement("li");
+        let text = document.createElement("span");
+        let btnContainer = createButtons(index);
+
+        if (todo.done) {
+            item.setAttribute("class", "item-done");
+        }
+
+
+        text.innerText = todo.text;
+
+        item.appendChild(text);
+        item.appendChild(btnContainer);
+        return item;
+    }
+
+    function createButtons(itemIndex) {
+        let btnContainer = document.createElement("div");
+        btnContainer.appendChild(createButton("&#9998;", setEditMode));
+        btnContainer.appendChild(createButton("&checkmark;", setDone));
+        btnContainer.appendChild(createButton("&times;", removeTodo));
+
+        return btnContainer;
+
+        function removeTodo(e) {
+            e.preventDefault();
+            todos.splice(itemIndex, 1);
+            refreshTodos();
+        }
+        function setDone() {
+            todos[itemIndex].done = !todos[itemIndex].done;
+            refreshTodos();
+        }
+
+        function setEditMode() {
+            todos[itemIndex].editMode = !todos[itemIndex].editMode;
+            refreshTodos();
+        }
+
+        function createButton(text, func) {
+            let btn = document.createElement("a");
+            btn.innerHTML = text;
+            btn.setAttribute("href", "#");
+            btn.onclick = func;
+            return btn;
+        }
+    }
+
+    function refreshTodos() {
+        while (todoList.firstChild) {
+            todoList.removeChild(todoList.firstChild)
+        }
+
+        todos.forEach((todo, index) => {
+            todoList.appendChild(createTodoItem(todo, index))
+        })
+    }
+}();
+
